313762bab611702fa88d540515174468f65d1ec2
Kristo Jeremy <kristojeremy@gmail.com>
2022-09-21 20:18:37 +0700

    Fix Deployment (6)

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -3,29 +3,27 @@ from django.core import serializers
 from django.shortcuts import render
 from mywatchlist.models import WatchlistItems
 
-pesan =''
-counter = 0
-watchlist_data = WatchlistItems.objects.all()
-
-for items in watchlist_data:
-    if(items.movie_watched == "Yes"):
-        counter+=1
-
-if(counter < 10 - counter):
-    pesan="Wah, kamu masih sedikit menonton!"
-else:
-    pesan="Selamat, kamu sudah banyak menonton!"
-
-context = {
-    'watchlist_items': watchlist_data,
-    'nama': 'Kristo Jeremy Thady Tobing',
-    'npm': '2106633310',
-    'pesan': pesan
-}
-
-
 
 def show_watchlist(request):
+    pesan =''
+    counter = 0
+    watchlist_data = WatchlistItems.objects.all()
+
+    for items in watchlist_data:
+        if(items.movie_watched == "Yes"):
+            counter+=1
+
+    if(counter < 10 - counter):
+        pesan="Wah, kamu masih sedikit menonton!"
+    else:
+        pesan="Selamat, kamu sudah banyak menonton!"
+
+    context = {
+        'watchlist_items': watchlist_data,
+        'nama': 'Kristo Jeremy Thady Tobing',
+        'npm': '2106633310',
+        'pesan': pesan
+    }
     return render(request, "watchlist.html", context)
 
 def show_xml(request):
